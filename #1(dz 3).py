aa2bf7591bc46eb173b1da8566c122de5ddf76a0
altkovev <55360336+altkovev@users.noreply.github.com>
2019-10-21 10:35:54 +0300

    Update #1(dz 3).py

--- #1(dz 3).py
+++ #1(dz 3).py
@@ -4,9 +4,9 @@ operation = ''.join(symbols & set(s))
 operation_index = s.find(operation)
 x, y = s[:operation_index], s[operation_index + 1:]
 
-if y.find(operation) != -1:
+if y[0] in ''.join(symbols):
     operation += operation
-    y in s[operation_index + 2:]
+    y = s[operation_index + 2:]
 
 x, y = float(x), float(y)
 operations = {
